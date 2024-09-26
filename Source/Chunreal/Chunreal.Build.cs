1f39b824fdd5a207e5711d545471132ace81f62d
Amir-BK <Amir.BK@gmail.com>
2024-09-26 13:37:17 +0300

    Syntax highlighter boilerplate

--- Source/Chunreal/Chunreal.Build.cs
+++ Source/Chunreal/Chunreal.Build.cs
@@ -50,7 +50,9 @@ public class Chunreal : ModuleRules
             {
                 "CoreUObject",
                 "Engine",
-            }
+				"Slate",
+				"SlateCore"
+			}
             );
     }
 }
